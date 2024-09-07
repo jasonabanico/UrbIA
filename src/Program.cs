875c44fdc889b32dcd90ee293681588ea189eee4
Jason Banico <jason.banico@recordpoint.com>
2024-09-07 11:48:53 +1000

    outputs

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,19 @@
 ﻿using Urbia.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace Urbia
 {
     public class Program
     {
-        private static Dictionary<string, string> _insightInputFiles = new Dictionary<string, string>()
+        private static List<(string, string)> _insightInputFiles = new List<(string, string)>()
         {
-            //{ "Safety Insights", "VICTORIAN_ROAD_CRASH_DATA - excerpt.geojson" },
-            { "Capacity Forcest Insights", "Traffic_Volume - excerpt.geojson" }
+            ( "Safety Insights", "VICTORIAN_ROAD_CRASH_DATA - excerpt.geojson" ),
+            ( "Capacity Forcest Insights", "Traffic_Volume - excerpt.geojson" ),
+            ( "Sustainability Insights", "Principal_Bicycle_Network_(PBN) - excerpt.geojson" ),
+            ( "Sustainability Insights", "train-passenger-service-counts.json" ),
+            ( "Public Utilisation Insights", "Mary Cairncross Scenic Reserve Dingtek PIR Counter - excerpt.txt" )
         };
 
         static void Main(string[] args)
@@ -33,14 +37,41 @@ namespace Urbia
 
             var insightsProcessor = serviceProvider.GetService<IInsightsProcessor>();
 
-            foreach (var insightType in _insightInputFiles.Keys)
+            var index = 0;
+            foreach (var insightInputFile in _insightInputFiles)
             {
-                var filename = _insightInputFiles[insightType];
-                Console.WriteLine($"Sample {insightType} from data {filename}.");
-                Console.WriteLine();
+                index++;
+                var insightType = insightInputFile.Item1;
+                var filename = insightInputFile.Item2;
+                var displayText = string.Empty;
+                displayText += $"Sample {insightType} from data {filename}." + System.Environment.NewLine;
+                displayText += System.Environment.NewLine;
                 (var data, var output) = insightsProcessor.ProcessInsightAsync(insightType, filename).Result;
-                Console.WriteLine($"Data: \n{data}");
-                Console.WriteLine($"{insightType} Suggestions: \n{output}");
+                displayText += $"Data: \n{data}" + System.Environment.NewLine;
+                displayText += System.Environment.NewLine;
+                displayText += $"{insightType} Suggestions: \n{output}";
+
+                Console.WriteLine(displayText);
+                WriteToFile($"insight-{index} {insightType}.txt", displayText);
+            }
+        }
+
+        private static void WriteToFile(string filename, string text)
+        {
+            try
+            {
+                var folderPath = $"{AppDomain.CurrentDomain.BaseDirectory}/Output Data";
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                var filePath = Path.Combine(folderPath, filename);
+                File.WriteAllText(filePath, text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
     }
